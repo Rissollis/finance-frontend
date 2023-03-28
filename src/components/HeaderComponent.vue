e9de846f83a6badb95c524ea6b79f01f3bfc95b8
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-03-28 14:12:53 -0300

    feat: iniciando projeto

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header id="header" class="w-full border-b border-bg-dourado">
+    <div
+      class="w-full max-w-2700px flex items-center justify-between px-20 py-5"
+    >
+      <div class="">
+        <router-link class="inline-block" to="/">
+          <img
+            class="w-full max-w-200px"
+            src="/icons/header-icon-dourado.png"
+            alt="icone finance"
+          />
+        </router-link>
+      </div>
+      <nav class="">
+        <ul class="uppercase flex">
+          <router-link
+            class="hover:bg-dourado px-10 py-5 active:bg-douradoActive mr-16 inline-block"
+            to="#"
+          >
+            <li class="tracking-widest">Arroz</li>
+          </router-link>
+          <router-link
+            class="hover:bg-dourado px-10 py-5 active:bg-douradoActive mr-16"
+            to="#"
+          >
+            <li class="tracking-widest">Arroz</li>
+          </router-link>
+          <router-link
+            class="hover:bg-dourado px-10 py-5 active:bg-douradoActive"
+            to="#"
+          >
+            <li class="tracking-widest">Arroz</li>
+          </router-link>
+        </ul>
+      </nav>
+      <div class="">
+        <button
+          class="bg-dourado hover:bg-douradoHover active:bg-douradoActive px-8 py-3"
+        >
+          Registrar-se
+        </button>
+      </div>
+    </div>
+  </header>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue, js, ts, jsx, tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        financePlex: ["IBM Plex Sans", "sans-serif;"],
+      },
+      maxWidth: {
+        "2700px": "168.75",
+        "200px": "8rem",
+      },
+      colors: {
+        blackBg: "#1A1A1A",
+        dourado: "#A5834E",
+        douradoHover: "#a5844ec9",
+        douradoActive: "#b88b42",
+      },
+    },
   },
   plugins: [],
 };
